e39d958d57fab217214e747e031f55d4faac8e65
June <1601745371@qq.com>
2023-10-28 21:31:17 +0800

    fix: 修复mac打包问题以及mac关闭后无法打开问题

--- electron/main/config/constants/winNames.ts
+++ electron/main/config/constants/winNames.ts
@@ -4,9 +4,9 @@
  * @Author: June
  * @Date: 2023-05-28 12:48:03
  * @LastEditors: June
- * @LastEditTime: 2023-05-28 12:51:05
+ * @LastEditTime: 2023-10-28 20:49:00
  */
 
 export const appMain = 'APPMAIN'
 export const tray2git = 'TRAT1GIT'
-export const appUpdate = 'APPUPDATe'
+export const appUpdate = 'APPUPDATE'
