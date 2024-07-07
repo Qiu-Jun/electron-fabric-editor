e980c66b1de5112c0ee0dd588547d9c093154219
June <1601745371@qq.com>
2024-07-07 14:17:29 +0800

    :bug: fix: 修复应用对开问题

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -16,7 +16,57 @@ import { appLoadUrl } from './config'
 
 import { initUpdate } from './utils/update'
 
+const getLock = app.requestSingleInstanceLock()
 let win: any = null
+
+function startApp() {
+  // 返回false 说明已有应用了
+  if (!getLock) {
+    win?.isMinimized() && win?.restore()
+    win?.focus()
+    return
+  } else {
+    app.quit()
+  }
+
+  app.whenReady().then(() => {
+    electronApp.setAppUserModelId('com.electron.june_fabric')
+
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window)
+    })
+
+    createWindow()
+    app.on('activate', () => {
+      // 当应用被激活时发出。 各种操作都可以触发此事件, 例如首次启动应用程序、
+      // 尝试在应用程序已运行时或单击应用程序的坞站或任务栏图标时重新激活它
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    initTray(win.getWin(appMain))
+    createMenu()
+    initUpdate(win)
+  })
+
+  // 除了 macOS 外，当所有窗口都被关闭的时候退出程序
+  app.on('window-all-closed', () => {
+    // process.platform Electron 目前只支持三个平台：win32 (Windows), linux (Linux) 和 darwin (macOS)
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  // 客户端聚焦
+  // app.on('browser-window-focus', () => { })
+
+  // // 客户端失去焦点
+  // app.on('browser-window-blur', () => { })
+
+  app.on('activate', () => win?.show())
+}
+
+startApp()
 
 function createWindow(): void {
   win = WinManage.initialize({
@@ -44,39 +94,3 @@ function createWindow(): void {
     moduleName: appMain
   })
 }
-
-app.whenReady().then(() => {
-  electronApp.setAppUserModelId('com.electron.june_fabric')
-
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
-
-  createWindow()
-  app.on('activate', () => {
-    // 当应用被激活时发出。 各种操作都可以触发此事件, 例如首次启动应用程序、
-    // 尝试在应用程序已运行时或单击应用程序的坞站或任务栏图标时重新激活它
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-
-  initTray(win.getWin(appMain))
-  createMenu()
-  initUpdate(win)
-})
-
-// 除了 macOS 外，当所有窗口都被关闭的时候退出程序
-app.on('window-all-closed', () => {
-  // process.platform Electron 目前只支持三个平台：win32 (Windows), linux (Linux) 和 darwin (macOS)
-  if (process.platform !== 'darwin') app.quit()
-})
-
-// 客户端聚焦
-// app.on('browser-window-focus', () => { })
-
-// // 客户端失去焦点
-// app.on('browser-window-blur', () => { })
-
-app.on('activate', () => win?.show())
