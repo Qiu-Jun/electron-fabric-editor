f85c4a1cb1e79a05105ce25adacff55c3635673c
June <1601745371@qq.com>
2024-07-27 20:30:01 +0800

    :hammer: refactor: ts文件整理

--- typings/env.d.ts
+++ typings/env.d.ts
@@ -2,13 +2,6 @@
 
 // import { Object } from 'fabric/fabric-impl';
 
-declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
-
 declare global {
   declare module 'fabric/fabric-impl' {}
 }
