8ac36e39c1aa6d5bcb5752113aae1ff8f2a576d8
June <1601745371@qq.com>
2024-07-27 20:32:00 +0800

    :hammer: refactor: ts迁移

--- typings/vite-env.d.ts
+++ typings/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+declare type Recordable<T = any> = Record<string, T>
+
+declare interface ViteEnv {
+  readonly APP_FLAG: string
+  readonly APP_TITLE: string
+  readonly APP_BASE_PATH: string
+  readonly APP_APIHOST: string
+  readonly APP_ADMINAPIHOST: string
+}
